3aa1be006e5af7a84653377f843e34637bf741a4
Nicki Ogg <oggnicki@yahoo.com>
2014-01-18 21:11:02 -0500

    Latest working version, Sat 9:10 pm

--- FindThatAddress/src/com/m4/purdue/mhacks/findthataddress/MainActivity.java
+++ FindThatAddress/src/com/m4/purdue/mhacks/findthataddress/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "On create main activity");
+        
     }
 
 
